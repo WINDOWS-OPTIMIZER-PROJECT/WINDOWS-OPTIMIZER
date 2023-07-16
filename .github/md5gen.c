d9e58e7735e285a9116e0e36112d3d0ce38119d5
GorillaGriller <126972636+GorillaGriller@users.noreply.github.com>
2023-07-16 17:04:15 +0200

    Create md5gen.c

---
+++ .github/md5gen.c
@@ -0,0 +1,34 @@
+5gen.c
+/* codesign make this parambane
+
+*/
+
+/* #author:gorillagriller*/
+/* #md5 generation of file content*/
+/* #status alpha*/
+/* #runtime posible: no*/
+@"
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FileHash
+{
+    public class HashChecker
+    {
+        public static bool CheckHash(string filePath, string storedHash, string algorithm)
+        {
+            using (var hashAlgorithm = HashAlgorithm.Create(algorithm))
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    byte[] hash = hashAlgorithm.ComputeHash(stream);
+                    string computedHash = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                    return computedHash == storedHash;
+                }
+            }
+        }
+    }
+}
+"@
